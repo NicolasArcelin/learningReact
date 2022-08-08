ee28308d0889c73f218a40aa6926270d32d45c47
NicolasArcelin <103464938+NicolasArcelin@users.noreply.github.com>
2022-08-08 14:08:11 -0400

    + Main Nav Bar, Meetups Page Improved

--- react-course-part2/src/App.js
+++ react-course-part2/src/App.js
@@ -1,21 +1,20 @@
-import { Route } from "react-router-dom";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import AllMeetupsPages from "./pages/AllMeetups";
+import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
-      <Route path="/">
-        <AllMeetupsPages />
-      </Route>
-      <Route path="/new-meetups">
-        <NewMeetupPage />
-      </Route>
-      <Route path="/favorites">
-        <FavoritesPage />
-      </Route>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/newmeetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
     </div>
   );
 }
